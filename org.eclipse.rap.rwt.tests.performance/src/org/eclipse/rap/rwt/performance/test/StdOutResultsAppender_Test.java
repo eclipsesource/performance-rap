4eaad3f6fbe0d4fb693f8123073ae6dd62724e0f
rsternberg <rsternberg@eclipsesource.com>
2011-02-07 12:50:01 +0100

    Moved tests to test bundle

---
+++ org.eclipse.rap.rwt.tests.performance/src/org/eclipse/rap/rwt/performance/test/StdOutResultsAppender_Test.java
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2011 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.rap.rwt.performance.test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import junit.framework.TestCase;
+
+import org.eclipse.rap.rwt.performance.MeasurementResults;
+import org.eclipse.rap.rwt.performance.file.StdOutResultsAppener;
+
+
+public class StdOutResultsAppender_Test extends TestCase {
+
+  private PrintStream bufferedSysOut;
+  private PrintStream bufferedSysErr;
+  private ByteArrayOutputStream sysOut;
+  private ByteArrayOutputStream sysErr;
+
+  @Override
+  protected void setUp() throws Exception {
+    bufferedSysOut = System.out;
+    bufferedSysErr = System.err;
+    sysOut = new ByteArrayOutputStream();
+    sysErr = new ByteArrayOutputStream();
+    System.setOut( new PrintStream( sysOut ) );
+    System.setErr( new PrintStream( sysErr ) );
+  }
+
+  @Override
+  protected void tearDown() throws Exception {
+    System.setOut( bufferedSysOut );
+    System.setErr( bufferedSysErr );
+  }
+
+  public void testAppendNull() {
+    StdOutResultsAppener appender = new StdOutResultsAppener();
+    try {
+      appender.append( this, (MeasurementResults)null );
+      fail();
+    } catch( NullPointerException e ) {
+      // expected
+    }
+  }
+
+  public void testAppendEmptyResult() {
+    StdOutResultsAppener appender = new StdOutResultsAppener();
+    MeasurementResults results = new MeasurementResults( new long[] { 0L } );
+    appender.append( this, results );
+    String expected = "Testcase\tAvg (ms)\tMed (ms)\n"
+                      + "StdOutResultsAppender_Test.testAppendEmptyResult\t"
+                      + "0.0\t0.0\n";
+    assertEquals( expected, sysOut.toString() );
+    assertEquals( "", sysErr.toString() );
+  }
+
+  public void testAppendTwoResults() {
+    StdOutResultsAppener appender = new StdOutResultsAppener();
+    long[] input1 = new long[] { 22000L, 55000L, 22000L };
+    appender.append( this, new MeasurementResults( input1 ) );
+    long[] input2 = new long[] { 20000L, 50000L, 20000L };
+    appender.append( this, new MeasurementResults( input2 ) );
+    String expected = "Testcase\tAvg (ms)\tMed (ms)\n"
+                      + "StdOutResultsAppender_Test.testAppendTwoResults\t"
+                      + "0.033\t0.022\n"
+                      + "StdOutResultsAppender_Test.testAppendTwoResults\t"
+                      + "0.03\t0.02\n";
+    assertEquals( expected, sysOut.toString() );
+    assertEquals( "", sysErr.toString() );
+  }
+}
---
+++ org.eclipse.rap.rwt.tests.performance/src/org/eclipse/rap/rwt/performance/test/StopWatch_Test.java
@@ -0,0 +1,76 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2011 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    EclipseSource - initial API and implementation
+ ******************************************************************************/
+package org.eclipse.rap.rwt.performance.test;
+
+import junit.framework.TestCase;
+
+import org.eclipse.rap.rwt.performance.StopWatch;
+import org.eclipse.rap.rwt.performance.MeasurementResults;
+
+
+public class StopWatch_Test extends TestCase {
+
+  public void testCreate() {
+    StopWatch stopWatch = new StopWatch();
+    MeasurementResults results = stopWatch.getResults();
+    assertEquals( 0, results.getNumberOfRuns() );
+    assertEquals( 0, results.getAllDurations().length );
+  }
+
+  public void testRunOnce() throws Exception {
+    StopWatch stopWatch = new StopWatch();
+    stopWatch.start();
+    Thread.sleep( 1 );
+    stopWatch.stop();
+    MeasurementResults results = stopWatch.getResults();
+    assertEquals( 1, results.getNumberOfRuns() );
+    assertEquals( 1, results.getAllDurations().length );
+    assertTrue( results.getAllDurations()[ 0 ] > 0 );
+  }
+
+  public void testRunTwice() throws Exception {
+    StopWatch stopWatch = new StopWatch();
+    stopWatch.start();
+    Thread.sleep( 1 );
+    stopWatch.stop();
+    stopWatch.start();
+    Thread.sleep( 2 );
+    stopWatch.stop();
+    MeasurementResults results = stopWatch.getResults();
+    assertEquals( 2, results.getNumberOfRuns() );
+    long[] allDurations = results.getAllDurations();
+    assertEquals( 2, allDurations.length );
+    assertTrue( allDurations[ 0 ] > 0 );
+    assertTrue( allDurations[ 1 ] > 0 );
+    assertTrue( allDurations[ 0 ] != allDurations[ 1 ] );
+  }
+
+  public void testStartTwice() {
+    StopWatch stopWatch = new StopWatch();
+    stopWatch.start();
+    try {
+      stopWatch.start();
+      fail( "StopWatch already running" );
+    } catch( IllegalStateException e ) {
+      // expected
+    }
+  }
+
+  public void testStopWithoutStarting() {
+    StopWatch stopWatch = new StopWatch();
+    try {
+      stopWatch.stop();
+      fail( "StopWatch not running" );
+    } catch( IllegalStateException e ) {
+      // expected
+    }
+  }
+}
